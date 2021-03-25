88541cb769ec814bb1beba047cf022084b03cf6c
mani333007 <mani333007@gmail.com>
2021-03-25 15:56:16 +0530

    githib repo link added on page

--- pages/index.js
+++ pages/index.js
@@ -58,6 +58,7 @@ export default function Home() {
           <TodoList todos={todos} updateTodo={updateTodo} deleteTodo={deleteTodo} />
         </div>
       </div>
+      <a href="https://github.com/Sai-Manikanta/todolist-nextjs">Code: https://github.com/Sai-Manikanta/todolist-nextjs</a>
     </div>
   )
 }
